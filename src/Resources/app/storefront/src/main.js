6d668bdeab31bdab781913d4428fe3110578c52b
Peter Roj <roj@digitalmanufaktur.com>
2021-05-25 15:49:22 +0200

    DmfManuTheme v2.0.0 - Shopware 6.4 compatibility

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -2,6 +2,7 @@ import InputSpinner from './js/input-spinner/input-spinner.plugin';
 import NewsletterPrivacy from './js/newsletter-privacy/newsletter-privacy.plugin';
 import TogglePassword from './js/toggle-password/toggle-password.plugin';
 import RegisterExpand from './js/register-expand/register-expand.plugin';
+import ExtendedWishlistWidgetPlugin from './js/wishlist-widget/wishlist-widget.plugin';
 
 const PluginManager = window.PluginManager;
 
@@ -9,3 +10,4 @@ PluginManager.register('NewsletterPrivacy', NewsletterPrivacy, '.footer-newslett
 PluginManager.register('InputSpinner', InputSpinner, '[data-input-spinner]');
 PluginManager.register('TogglePassword', TogglePassword, '[data-toggle-password]');
 PluginManager.register('RegisterExpand', RegisterExpand, '[data-register-expand]');
+PluginManager.override('WishlistWidget', ExtendedWishlistWidgetPlugin, '[data-wishlist-widget]');
